6da8577ba21b9ea80c49440206aa4d23154579b3
Machoka Daisy <machokadaisy25@gmail.com>
2020-09-13 17:04:05 +0300

    add a methid that adds credentials to credentials list

--- credentials.py
+++ credentials.py
@@ -2,6 +2,8 @@ class Credentials:
     '''
     Class that generates new instances of credentials
     '''
+    credentials_list = []
+
     def __init__(self, account, username, password):
         '''
         __init__ method that helps us define properties for credential objects
@@ -15,3 +17,9 @@ class Credentials:
         self.account = account
         self.username = username
         self.password = password
+
+    def save_credentials(self):
+        '''
+        save_credentials method to save objects into credentials list
+        '''
+        Credentials.credentials_list.append(self)

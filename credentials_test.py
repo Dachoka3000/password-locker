260bc662e31717b0e1eedf4f537f7f4be64da467
Machoka Daisy <machokadaisy25@gmail.com>
2020-09-13 17:13:08 +0300

    add a test to check if we can save multiple credentials to the credentials list

--- credentials_test.py
+++ credentials_test.py
@@ -31,6 +31,15 @@ class TestCredentials(unittest.TestCase):
         self.new_credentials.save_credentials()
         self.assertEqual(len(Credentials.credentials_list), 1)
 
+    def test_save_multiple_credentials(self):
+        '''
+        test_save_multiple_credentials to check if we can saave multiple credentials
+        '''
+        self.new_credentials.save_credentials()
+        test_credential = Credentials("Instagram", "Muse Art", "museart1000")
+        test_credential.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 2)
+
 if __name__ == '__main__':
     unittest.main()
 
